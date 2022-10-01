6f2694452f88b3cdea4f09218aa83d8326505bb2
Menyus11 <senkialfonz11@gmail.com>
2022-10-01 23:17:20 +0200

    react learning

--- src/components.js
+++ src/components.js
@@ -1,24 +1,16 @@
 import React from "react";
+import { useState } from "react";
 
 const Buttons = (props) => {
 
-    const operationDisabled = () => {
-        document.querySelectorAll('.operation').forEach( e => {
-            e.setAttribute('disabled', "")
-        });
-    }
-
-    const operationEnabled = () => {
-        document.querySelectorAll('.operation').forEach( e => {
-            e.removeAttribute('disabled')
-        });
-    }
+    const [isActive, setActive] = useState(true);
 
     const buttonsHandler = props.getDisplayData === 0 ?
-        (event) => { props.setDisplayData(event.target.value); operationEnabled() } :
-        function (event) { props.setDisplayData(props.getDisplayData + event.target.value); operationEnabled() };
 
-    const operatorHandler = function (event) { props.setDisplayData(props.getDisplayData + event.target.value); operationDisabled() };
+        (event) => { props.setDisplayData(event.target.value); setActive(true) } :
+        function (event) { props.setDisplayData(props.getDisplayData + event.target.value); setActive(true) };
+
+    const operatorHandler = function (event) { props.setDisplayData(props.getDisplayData + event.target.value); setActive(false) };
 
 
     const NumberButton = (props) => {
@@ -38,50 +30,47 @@ const Buttons = (props) => {
             /* eslint-disable-next-line */
             <button value="=" className='col-10 bg-success calcbutton m-2 py-4 rounded' onClick={() => { props.setDisplayData(eval(props.getDisplayData)) }}>=</button>
         )
-    } 
+    }
 
     const ResetButton = () => {
         return (
-            /* eslint-disable-next-line */
-            <button className='col bg-danger calcbutton m-2 py-4 rounded' onClick={() => { props.setDisplayData(0); operationEnabled() }}>C</button>
+            <button className='col bg-danger calcbutton m-2 py-4 rounded' onClick={() => { props.setDisplayData(0); setActive(true) }}>C</button>
         )
-    } 
-    
-const buttonsArray = [
-    <NumberButton value="7" />,
-    <NumberButton value="8" />,
-    <NumberButton value="9" />,
-    <OperationButton value="+" className="operation"/>,
-    <NumberButton value="4" />,
-    <NumberButton value="5" />,
-    <NumberButton value="6" />,
-    <OperationButton value="-" className="operation"/>,
-    <NumberButton value="1" />,
-    <NumberButton value="2" />,
-    <NumberButton value="3" />,
-    <OperationButton value="*" className="operation"/>,
-    <ResetButton />,
-    <NumberButton value="0" />,
-    <OperationButton value="." className="operation" />,
-    <OperationButton value="/" className="operation" />,
-    <EqualButton/>
-];
-    
+    }
 
-
-    /* buttonsArray.filter(element => { if(element.type.name === "OperationButton") {element.setAttribute("disabled", "") }}) */
+    const buttonsArray = [
+        <NumberButton value="7" />,
+        <NumberButton value="8" />,
+        <NumberButton value="9" />,
+        <OperationButton value={isActive ? "+" : ""} />,
+        <NumberButton value="4" />,
+        <NumberButton value="5" />,
+        <NumberButton value="6" />,
+        <OperationButton value={isActive ? "-" : ""} />,
+        <NumberButton value="1" />,
+        <NumberButton value="2" />,
+        <NumberButton value="3" />,
+        <OperationButton value={isActive ? "*" : ""} />,
+        <ResetButton />,
+        <NumberButton value="0" />,
+        <OperationButton value={isActive ? "." : ""} />,
+        <OperationButton value={isActive ? "/" : ""} />,
+        <EqualButton />
+    ];
 
     return (
         <div className='p-3' >
 
             <div className='row row-cols-5 justify-content-center'>
 
-                {buttonsArray.map((buttonElement, id) => {return(
-                    <React.Fragment key={id}>
-                        {buttonElement}
-                    </React.Fragment>                   
-                )} )}
-                
+                {buttonsArray.map((buttonElement, id) => {
+                    return (
+                        <React.Fragment key={id}>
+                            {buttonElement}
+                        </React.Fragment>
+                    )
+                })}
+
             </div>
 
         </div>
